3311768791feedff0dcfd371866d41d17355a427
naseeb-helali <150822279+naseeb-helali@users.noreply.github.com>
2025-10-12 02:34:53 +0300

    Create providers.tf

---
+++ iac/terraform/providers.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.region
+}
+
+# Latest Amazon Linux 2 (x86_64)
+data "aws_ami" "al2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
